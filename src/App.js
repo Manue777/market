1404c04b44cf23c2d6559e723a54b12dca189a25
manue777 <lejuene.manu@gmail.com>
2022-11-24 21:22:33 -0500

    MainLayout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { AuthProvider } from "./context/AuthContext";
 import Router from "./routes";
-
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
-  return (
-    <div className="App">
-      <AuthProvider>
-         <Router />
-      </AuthProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<AuthProvider>
+				<Router />
+			</AuthProvider>
+		</div>
+	);
 };
 
 export default App;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,49 +1,40 @@
 import { createContext, useState } from "react";
 
-
 export const AuthContext = createContext();
 
-// Crear un Provider: Este provider a proveer la variables y funciones
-// que creemos
 export const AuthProvider = (props) => {
-  // La palabra children hace referencia a los components hijo
-  // y este children viaja por props
-  const { children } = props;
+	const { children } = props;
 
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem("user")) ?? {}
-  );
+	const [user, setUser] = useState(
+		JSON.parse(localStorage.getItem("user")) ?? {}
+	);
 
-  function authlogin (user){
-    if (!user) return false;
+	function authLogin(user) {
+		if (!user) return false;
+		localStorage.setItem("user", JSON.stringify(user));
+		setUser(user);
+		return true;
+	}
 
-    localStorage.setItem("user", JSON.stringify(user));
-    setUser(user);
-    return true;
-  }
+	function logout() {
+		localStorage.clear();
+		setUser({});
+	}
 
-  
+	function isAuth() {
+		return Object.entries(user).length !== 0;
+	}
 
-  function logout() {
-    localStorage.clear();
-    setUser({});
-  }
-
-  // funcion para validar si la session existe
-  function isAuth() {
-    return Object.entries(user).length !== 0;
-  }
-
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        authlogin,
-        logout,
-        isAuth,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				authLogin,
+				logout,
+				isAuth,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
